cee0bd78267a0480a24e5c5597032299d3069e84
florian_engels <engels.florian@yahoo.de>
2020-07-06 10:35:41 +0200

    Erstellung beispielhafter Admittance Matrix

---
+++ Admittance Matrix.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pypsa
+
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+plt.style.use("bmh")
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[3]:
+
+
+n = pypsa.Network("elec_s_5_ec_lcopt_Co2L-24H.nc")
+n.plot();
+
+
+# ## admittance Matrix
+
+# In[4]:
+
+
+n.determine_network_topology()
+
+
+# In[6]:
+
+
+for sub in n.sub_networks.obj:
+    pypsa.pf.calculate_Y(sub)
+
+
+# In[8]:
+
+
+print(sub.Y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
--- make_network.sh
+++ make_network.sh
@@ -6,4 +6,4 @@ cp config.tutorial.yaml config.yaml
 conda activate pypsa-eur
 
 # Erzeugen eines 50Knoten Beispielnetzwerks
-snakemake -j 1 results/networks/elec_s_50_ec_lcopt_Co2L-24H.nc
+snakemake -j 1 results/networks/elec_s_5_ec_lcopt_Co2L-24H.nc
